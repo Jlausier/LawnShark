1c7dbd53ea3053dad80b9f429abd87eb85b3fd8d
Brian Gorman <briangorman99@gmail.com>
2023-09-09 17:19:34 -0400

    add graphql typedefs

--- server/models/Service.js
+++ server/models/Service.js
@@ -1,16 +1,14 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const serviceSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true
-    },
-  }
-);
+const serviceSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+});
 
-const User = model('Service', serviceSchema);
+const Service = model("Service", serviceSchema);
 
 module.exports = Service;
