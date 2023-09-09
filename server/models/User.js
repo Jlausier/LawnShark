fd75ddca6fa89e227d0bf4e590f8fc4a045588da
Brian Gorman <briangorman99@gmail.com>
2023-09-09 14:38:04 -0400

    add Company model, connect User model roles

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-const reviewSchema = new Schema({
-  reviewText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  rating: {
-    type: Number,
-    maxlength: 1,
-    match: /[0-5]/,
-  },
-});
-
-const companySchema = new Schema({
-  description: {
-    type: String,
-    required: "You need to leave a description!",
-    minlength: 1,
-    maxlength: 500,
-    trim: true,
-  },
-  services: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Service",
-    },
-  ],
-  reviews: [reviewSchema],
-});
-
 const userSchema = new Schema({
   name: {
     type: String,
@@ -53,13 +18,8 @@ const userSchema = new Schema({
     required: true,
     minlength: 8,
   },
-  isCompany: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  customer: customerSchema,
-  company: companySchema,
+  _customer: { type: Schema.Types.ObjectId, ref: "Customer" },
+  _company: { type: Schema.Types.ObjectId, ref: "Company" },
 });
 
 //password logic
@@ -74,6 +34,10 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.getRole = function () {
+  return this._company ? "company" : "customer";
 };
 
 const User = model("User", userSchema);
