1488631c7e1e13288e4b9bbb0ed39764d001ccfe
holdenmg <holdenmgarrison@gmail.com>
2023-09-09 18:23:28 -0400

    Added addUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,24 @@ const { Service, User, Posting, Bid } = require("../models");
 const resolvers = {
   Query: {
     services: async () => {
-      return Service.find({});
+      return await Service.find({});
     },
+    postings: async() => {
+      return await Posting.find({})
+    }
   },
-};
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      // First we create the user
+      const user = await User.create({ username, email, password });
+      // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+  }
+}
+
 
 module.exports = resolvers;
