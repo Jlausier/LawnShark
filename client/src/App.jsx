2bae39eaa4858c599404007350a9a9723eded9c0
Brian Gorman <briangorman99@gmail.com>
2023-09-09 16:32:14 -0400

    add AuthContext and AuthProvider, add AuthProvider to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { Outlet } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import TopBar from './components/TopBar';
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import TopBar from "./components/TopBar";
+
+import { AuthProvider } from "./utils/AuthProvider";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       {/* TODO: create a CSS GRID or Bootstrap Grid System to arrange the divs */}
-      <main className='min-vh-100 row gx-0'>
-        <div className=' col-sm-4 col-md-3 col-lg-2 gx-0'>
+      <main className="min-vh-100 row gx-0">
+        <div className=" col-sm-4 col-md-3 col-lg-2 gx-0">
           <NavBar />
         </div>
-        <div className=' col-sm-8 col-md-9 col-lg-10 gx-0'>
-          <div className=' row gx-0'>
+        <div className=" col-sm-8 col-md-9 col-lg-10 gx-0">
+          <div className=" row gx-0">
             <TopBar />
           </div>
-          <div className=' row gx-0'>
-            <Outlet />Outlet
+          <div className=" row gx-0">
+            <Outlet />
+            Outlet
           </div>
         </div>
       </main>
-    </>
+    </AuthProvider>
   );
 }
 
