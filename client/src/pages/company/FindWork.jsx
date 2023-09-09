8767b5b07fef5f5be2f40932570463e1bd2bddd2
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-09 17:08:58 -0400

    find work formatted

--- client/src/pages/company/FindWork.jsx
+++ client/src/pages/company/FindWork.jsx
@@ -1,15 +1,24 @@
+import BidCard from '../../components/BidCard';
+
 export default function FindWork() {
 
     return (
       <>
         <div className="border p-5">
-          <h2 className="border mb-5 fs-1">Find Work</h2>
+          <div className='mb-5'>
+            <h2 className="border fs-1">Find Work</h2>
+            <span className=''>Click on job titles to view more details</span>
+          </div>
           <div className="border mb-3">
-            <span>Filter Results</span>
-            <a></a>
+            <span className='pe-2'>Filter Results:</span>
+            <a className='pe-2 '>Lawn Work</a>
+            <a className='pe-2'>Tree Trimming</a>
+            <a className='pe-2'>Power Washing</a>
+            <a className='pe-2'>Fertilizing</a>
           </div>
           <div className="border">
             <BidCard />
+            Cards
           </div>
         </div>
       </>
--- client/src/pages/company/MyBids.jsx
+++ client/src/pages/company/MyBids.jsx
@@ -1,8 +1,24 @@
 export default function MyBids() {
 
-    return (
-      <>
-        <div>My Bids</div>
-      </>
-    );
+  return (
+    <>
+      <div className="border p-5">
+        <div className='mb-5'>
+          <h2 className="border fs-1">My Bids</h2>
+          <span className=''>Click on job titles to view more details</span>
+        </div>
+        <div className="border mb-3">
+          <span className='pe-2'>Filter Results:</span>
+          <a className='pe-2 '>Lawn Work</a>
+          <a className='pe-2'>Tree Trimming</a>
+          <a className='pe-2'>Power Washing</a>
+          <a className='pe-2'>Fertilizing</a>
+        </div>
+        <div className="border">
+          {/* <BidCard /> */}
+          Cards
+        </div>
+      </div>
+    </>
+  );
 }
