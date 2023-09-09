50743b7c3aca271558c560164bcad06f3f6628dc
Brian Gorman <briangorman99@gmail.com>
2023-09-09 15:23:53 -0400

    update naming conventions

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,23 +1,28 @@
 export default function NavBar() {
-
-    return (
-      <>
-        <div className="min-vh-100 p-4 bg-dark text-white">
-          <div className="">Logo</div>
-          <nav className="navbar navbar-expand-lg">
+  return (
+    <>
+      <div className="min-vh-100 p-4 bg-dark text-white">
+        <div className="">Logo</div>
+        <nav className="navbar navbar-expand-lg">
           <ul className="navbar-nav nav-pills flex-column">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Find Work</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">My Bids</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Search</a>
-                    </li>
-                </ul>
-          </nav>
-        </div>
-      </>
-    );
-};
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Find Work
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                My Bids
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Search
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
