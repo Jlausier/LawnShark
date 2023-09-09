6551fc2315b41debe8db0b1db1df787003d1ece4
holdenmg <holdenmgarrison@gmail.com>
2023-09-09 09:16:17 -0400

    Added package.json to server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,29 +1,5 @@
 const typeDefs = `
-  type Posting {
-    _id: ID
-    name: String
-    price: Int
-    location: String
-    bids: [Bid]
-  }
-
-  type Bid {
-    _id: ID
-    amount: Int
-    posting: Posting
-    company: Company
-  }
-
-  type Company {
-    _id: ID
-    name: String
-    bids: [Bid]
-  }
-
-  type Query {
-    postings: [Posting]
-    
-  }
+ 
 `;
 
 module.exports = typeDefs;
