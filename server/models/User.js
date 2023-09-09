9c350ccff556a9ba59f5fc22875d98862489d675
Brian Gorman <briangorman99@gmail.com>
2023-09-09 14:15:48 -0400

    extract companySchema and customerSchema from userSchema

--- server/models/User.js
+++ server/models/User.js
@@ -1,88 +1,94 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true
+const customerSchema = new Schema({
+  location: [
+    {
+      adress: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      zip: {
+        type: String,
+        required: true,
+      },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!'],
+  ],
+  postings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Posting",
     },
-    password: {
-      type: String,
-      required: true,
-      minlength: 8,
+  ],
+});
+
+const companySchema = new Schema({
+  description: {
+    type: String,
+    required: "You need to leave a description!",
+    minlength: 1,
+    maxlength: 500,
+    trim: true,
+  },
+  services: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Service",
     },
-    customer: [
-      { 
-        location: [{
-          adress: {
-            type: String,
-            required: true
-          },
-          city: {
-            type: String,
-            required: true
-          },
-          state: {
-            type: String,
-            required: true
-          },
-          zip: {
-            type: String,
-            required: true
-          }
-        }],
-        postings: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Posting'
-        }]
-      }],
+  ],
+  reviews: [
+    {
+      reviewText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      rating: {
+        type: Number,
+        maxlength: 1,
+        match: /[0-5]/,
+      },
+    },
+  ],
+});
 
-    company: [{
-        description: {
-          type: String,
-          required: 'You need to leave a description!',
-          minlength: 1,
-          maxlength: 500,
-          trim: true,
-        },
-        services: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Service'
-          }],
-        reviews: [{
-            reviewText: {
-              type: String,
-              required: true,
-              minlength: 1,
-              maxlength: 280,
-            },
-            createdAt: {
-              type: Date,
-              default: Date.now,
-              
-            },
-            rating: {
-              type: Number,
-              maxlength: 1,
-              match: /[0-5]/
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  customer: customerSchema,
+  company: companySchema,
+});
 
-            }
-
-          }]
-      }]
-  });
-  
 //password logic
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -90,11 +96,10 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
