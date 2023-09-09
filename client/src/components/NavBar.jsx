949f648bc30fd63a0f99c6d982c01dde92dac0e0
Jacob <Jakelausier7878@gmail.com>
2023-09-09 17:57:40 -0400

    pretty buttons

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,24 +1,18 @@
 export default function NavBar() {
   return (
     <>
-      <div className="min-vh-100 p-4 bg-dark text-white">
-        <div className="">Logo</div>
+      <div className="min-vh-100 p-4 bg-success text-white ">
+        <div className="pb-2">Logo</div>
         <nav className="navbar navbar-expand-lg">
           <ul className="navbar-nav nav-pills flex-column">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Find Work
-              </a>
+            <li className="nav-item active pb-2">
+            <button type="button" class="btn btn-secondary">Find Work</button>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                My Bids
-              </a>
+            <li className="nav-item pb-2">
+            <button type="button" class="btn btn-secondary">My bids</button>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Search
-              </a>
+            <li className="nav-item pb-2">
+            <button type="button" class="btn btn-secondary">Search</button>
             </li>
           </ul>
         </nav>
