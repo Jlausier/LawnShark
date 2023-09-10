6b5a5731cc7c8831b791ecd02b006b353bbf89fb
Brian Gorman <briangorman99@gmail.com>
2023-09-10 18:17:36 -0400

    add add_user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,15 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($email: String!, $password: String!) {
+    addUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+      }
+    }
+  }
+`;
