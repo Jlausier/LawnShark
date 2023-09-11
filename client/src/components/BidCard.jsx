634394e7199fe1b8405e9a621326a730de593be5
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-11 15:24:18 -0400

    cleaned up the bid card

--- client/src/components/BidCard.jsx
+++ client/src/components/BidCard.jsx
@@ -29,19 +29,36 @@ export default function BidCard() {
   }
 
   return (
-  <div className="card">
-    <div className="card-body">
-      <div className="row mt-2 mb-4">
-        <div className="col-6">
-        <h5 className="card-title">{bid.posting.title}</h5>
+    <div className="card w-100">
+      <div className="card-body">
+        <div className="row mt-2 mb-4">
+          <div className="col-6">
+            <h5 className="card-title fs-3">{bid.posting.title}</h5>
+          </div>
+          <div className="col-6 d-flex justify-content-end align-items-center">
+            {/* Make into a Link */}
+            <a href="/Messages" className="mx-2">Message</a>
+            {/* Make button a component */}
+            {/* Add a modal */}
+            <span className="px-4 py-2 rounded green text-light fs-5">
+              ${bid.posting.askingPrice}
+            </span>
+          </div>
         </div>
+        <div className="row pb-3">
+          <div className="col-4">
+            <span className="card-text">Client: { bid.posting.customer.name }</span>
+          </div>
+          <div className="col-4">
+            <span className="card-text">Location: { bid.posting.customer.location }</span>
+          </div>
+          <div className="col-4">
+            <span className="card-text">Frequecny: { bid.posting.frequency }</span>
+          </div>
+        </div>
+        <span>Description:</span>
+        <p className="card-text">{ bid.posting.description }</p>
       </div>
-      
-      <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="card-link">Card link</a>
-      <a href="#" className="card-link">Another link</a>
     </div>
-  </div>
   );
 }
