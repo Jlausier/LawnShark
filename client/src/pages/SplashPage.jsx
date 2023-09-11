96f069a3d516ff3016cfb97c027333b633d1457e
Jacob <Jakelausier7878@gmail.com>
2023-09-11 13:00:14 -0400

    css'd the login page

--- client/src/pages/SplashPage.jsx
+++ client/src/pages/SplashPage.jsx
@@ -1,19 +1,21 @@
 // import LoginForm from '../components/LoginForm'
 // import SignUpLink from '../components/SignUpLink'
-
+import "../index.css";
 
 export default function SplashPage() {
 
     return (
-      <div className="min-vh-100 green d-flex justify-content-center align-items-center">
-        <div className="row">
-          <div className="col">
+     
+      <div className="min-vh-100 green d-flex justify-content-center align-items-center bg-pic">
+       
+        <div className="row  rounded">
+          <div className="col motto rounded border border-1">
             <span>Welcome to</span>
             <h1>Lawn Shark</h1>
-            <span>Company Slogan</span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a tincidunt magna. </p>
+            <span className="pb-4">Make the proffesionals come to you!</span>
+            <p>Input a bid on the work you need done, and the profeesionals will come to you. </p>
           </div>
-          <div className="p-4 border border-1 rounded bg-light col d-flex flex-column">
+          <div className=" p-4 border border-1 rounded motto2 col d-flex flex-column">
             <h2 className="fs-5">Log in to Lawn Shark</h2>
             {/* Move Into A Component */}
             {/* <LoginForm /> */}
@@ -21,14 +23,14 @@ export default function SplashPage() {
               <div className="mb-3">
                 <label for="exampleInputEmail1" className="form-label">Email address</label>
                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <div id="emailHelp" className="form-text text-white">We'll never share your email with anyone else.</div>
               </div>
 
               <div className="mb-3">
                 <label for="exampleInputPassword1" className="form-label">Password</label>
                 <input type="password" className="form-control" id="exampleInputPassword1" />
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-secondary">Submit</button>
             </form>
 
             <hr/>
@@ -43,5 +45,7 @@ export default function SplashPage() {
           </div>
         </div>
       </div>
+
+      
     );
 }
