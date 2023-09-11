8e156316a2f4279e0414c217c2dcb28c029f7678
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-11 11:15:04 -0400

    added temporary nav links to access all pages

--- client/src/components/nav/NavBar.jsx
+++ client/src/components/nav/NavBar.jsx
@@ -7,6 +7,10 @@ export default function NavBar() {
   const currentPage = useLocation().pathname;
   const links = [
     {
+      title: "Company Profile",
+      path: "/CompanyProfile",
+    },
+    {
       title: "Find Work",
       path: "/FindWork",
     },
@@ -19,16 +23,28 @@ export default function NavBar() {
       path: "/MyBids",
     },
     {
-      title: "Search",
-      path: "/Search",
+      title: "Customer Profile",
+      path: "/UserProfile",
     },
     {
       title: "Job Postings",
       path: "/JobPostings",
     },
     {
+      title: "Single Job Posting",
+      path: "/JobPosting",
+    },
+    {
       title: "Search",
       path: "/Search",
+    },
+    {
+      title: "Notifications",
+      path: "/Notifications",
+    },
+    {
+      title: "Messages",
+      path: "/Messages",
     },
   ];
 
