33443685ef74787242d1f5c1f2ef69acdc4589b3
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-11 15:48:15 -0400

    company card start

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -14,12 +14,15 @@ export default function Search() {
         <div className="d-flex flex-column">
           {/* Turn search form into a seperate component */}
           {/* <SearchForm /> */}
-          <form class="d-flex w-100" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-          <CompanyCard />
-          // Company Card goes here
+          <div className="mb-5">
+            <form class="d-flex w-100" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+          <div>
+            <CompanyCard />
+          </div>
         </div>
       </div>
     </div>
