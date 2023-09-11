e895deeabd51aa4050921170fc2a2cc40288bd34
Brian Gorman <briangorman99@gmail.com>
2023-09-11 14:49:34 -0400

    fix connection between client and server, fix jsx classNames, add scripts to package.json, add logging plugin to apollo client

--- client/src/pages/user/JobPostings.jsx
+++ client/src/pages/user/JobPostings.jsx
@@ -1,28 +1,31 @@
+/* eslint-disable no-unused-vars */
 import { useLocation } from "react-router-dom";
-import JobPostingCard from  '../../components/Button';
-import Button from  '../../components/JobPostingCard';
+import JobPostingCard from "../../components/Button";
+import Button from "../../components/JobPostingCard";
 
 export default function JobPostings() {
-
   const currentPage = useLocation().pathname;
-  const link = 
-    {
-      title: "Add New",
-      path: "/CreateJobPosting",
-    }
+  const link = {
+    title: "Add New",
+    path: "/CreateJobPosting",
+  };
 
   return (
-      <div className='p-5'>
-        <div className="border p-4 rounded">
-          <div className='mb-5 d-flex justify-content-between align-items-start'>
-            <h2 className=' fs-1'>My Job Postings</h2>
-            <a class='btn green text-light' href='/CreateJobPosting' role='button'>New Job Posting</a>
-            {/* <Button {...link} currentPage={currentPage} key={link.path} /> */}
-          </div>
-          <div className=''>
-            <JobPostingCard />
-            // Cards Go Here
-          </div>
+    <div className="p-5">
+      <div className="border p-4 rounded">
+        <div className="mb-5 d-flex justify-content-between align-items-start">
+          <h2 className=" fs-1">My Job Postings</h2>
+          <a
+            className="btn green text-light"
+            href="/CreateJobPosting"
+            role="button"
+          >
+            New Job Posting
+          </a>
+          {/* <Button {...link} currentPage={currentPage} key={link.path} /> */}
+        </div>
+        <div className="">
+          <JobPostingCard />- // Cards Go Here
         </div>
       </div>
     </div>
--- client/src/utils/apolloClient.js
+++ client/src/utils/apolloClient.js
@@ -1,5 +1,21 @@
 import { createHttpLink, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const loggingPlugin = {
+  async requestDidStart(requestContext) {
+    console.log("Request started! Query:\n" + requestContext.request.query);
+
+    return {
+      async parsingDidStart(requestContext) {
+        console.log("Parsing started!\n" + requestContext);
+      },
+
+      async validationDidStart(requestContext) {
+        console.log("Validation started\n" + requestContext);
+      },
+    };
+  },
+};
+
 const link = createHttpLink({
   uri: "/graphql",
   credentials: "same-origin",
@@ -8,6 +24,7 @@ const link = createHttpLink({
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link,
+  plugins: [loggingPlugin],
 });
 
 export default client;
