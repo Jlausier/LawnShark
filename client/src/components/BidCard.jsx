b13a75496627f6bcf892a8f2f7cee4b5588cd66e
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-11 15:11:51 -0400

    bid styling

--- client/src/components/BidCard.jsx
+++ client/src/components/BidCard.jsx
@@ -1,10 +1,42 @@
 import "../index.css";
 
 export default function BidCard() {
+
+  const bid = {
+    _id: "",
+    amount: 0,
+    posting: {
+      _id: "",
+      title: "#postingTitle",
+      service: {
+        _id: "",
+        name: "#serviceName",
+      },
+      askingPrice: 0,
+      estimatePrice: 0,
+      customer: {
+        _id: "",
+        name: "#customerName",
+        location: "#customerLocation"
+      },
+      description: "#description",
+      frequency: "#frequency",
+    },
+    company: {
+      _id: "",
+      name: "",
+    },
+  }
+
   return (
   <div className="card">
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
+      <div className="row mt-2 mb-4">
+        <div className="col-6">
+        <h5 className="card-title">{bid.posting.title}</h5>
+        </div>
+      </div>
+      
       <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" className="card-link">Card link</a>
