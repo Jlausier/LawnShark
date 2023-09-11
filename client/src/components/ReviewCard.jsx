78c3fea0254b573e4fd4794e9893e9d33cbecce0
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-11 16:42:35 -0400

    review card

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,7 +1,30 @@
 export default function ReviewCard() {
+
+  const review ={
+      reviewText: "#reviewText",
+      createdAt: "#createdAt",
+      rating: 5,
+      customer: {
+        _id: "",
+        name: "#customerName",
+        location: "#customerLocation"
+      },
+  }
+
     return (
-      <>
-        <></>
-      </>
+      <div class="card w-100">
+        <div class="card-body">
+          <div className="row mt-2 mb-4">
+            <div className="col-6">
+              <h5 class="card-title fs-5">{ review.customer.name }</h5>
+              <h6 className="card-subtitle fs-6 text-muted">{ review.createdAt }</h6>
+            </div>
+            <div className="col-6 d-flex justify-content-end align-items-center">
+              <span className="px-4 py-2 rounded green text-light fs-6">{ review.rating }</span>
+            </div>
+          </div>
+          <p class="card-text">{ review.reviewText }</p>
+        </div>
+      </div>
     );
   }
