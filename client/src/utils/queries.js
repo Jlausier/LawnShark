605436dc2937e635a2482170b977736d138bea10
Brian Gorman <briangorman99@gmail.com>
2023-09-10 22:24:57 -0400

    add posting client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,4 +14,43 @@ export const QUERY_SERVICE = gql`
       name
     }
   }
+
+  query posting($postingId) {
+    posting(postingId: $postingId) {
+      _id
+      service {
+        _id
+        name
+      }
+      askingPrice
+      estimatePrice
+      bids {
+        _id
+        amount
+        company {
+          _id
+          name
+        }
+      }
+      customer {
+        _id
+        name
+      }
+    }
+  }
+
+  query postings {
+    postings {
+      _id
+      service {
+        _id
+        name
+      }
+      askingPrice
+      customer {
+        _id
+        name
+      }
+    }
+  }
 `;
