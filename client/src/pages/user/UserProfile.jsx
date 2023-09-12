2ae94e4ae10b7e20fd428534306fd20d8ec060af
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-12 19:14:58 -0400

    started modal component

--- client/src/pages/user/UserProfile.jsx
+++ client/src/pages/user/UserProfile.jsx
@@ -76,35 +76,35 @@ export default function UserProfile() {
         </div>
         {showModal && (
           <div className="modal show" tabIndex="-1" style={{ display: 'block' }}>
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">Edit Profile</h5>
-                  <button
-                    type="button"
-                    className="btn-close"
-                    aria-label="Close"
-                    onClick={closeModal}
-                  ></button>
-                </div>
-                <div className="modal-body">
-                  {/* Content for your modal */}
-                  <p>Modal body text goes here.</p>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={closeModal}
-                  >
-                    Close
-                  </button>
-                  <button type="button" className="btn btn-primary" onClick={closeModal}>
-                    Save changes
-                  </button>
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Edit Profile</h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      aria-label="Close"
+                      onClick={closeModal}
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    {/* Content for your modal */}
+                    <p>Modal body text goes here.</p>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={closeModal}
+                    >
+                      Close
+                    </button>
+                    <button type="button" className="btn btn-primary" onClick={closeModal}>
+                      Save changes
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
           </div>
         )}
       </div>
