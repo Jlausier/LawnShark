f2a4342a93347fdf8035434b8df891f0221f29ef
Jacob <Jakelausier7878@gmail.com>
2023-09-12 14:56:24 -0400

    added conversations box

---
+++ client/src/components/message/MessageConversation.jsx
@@ -0,0 +1,16 @@
+function MessageConvo() {
+    return (
+        <div className="card">
+        <div className="card-header">
+          Conversations
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Brian</li>
+          <li className="list-group-item">Eric</li>
+          <li className="list-group-item">Matthew</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default MessageConvo;
