e39e9a26bbb1e5246f1846dcea1878236ecdda97
Brian Gorman <briangorman99@gmail.com>
2023-09-11 23:22:11 -0400

    add toJSON properties to mongoose schemas

--- server/models/Company.js
+++ server/models/Company.js
@@ -22,28 +22,35 @@ const reviewSchema = new Schema({
   },
 });
 
-const companySchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: "You need to leave a description!",
-    minlength: 1,
-    maxlength: 500,
-    trim: true,
-  },
-  services: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Service",
+const companySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-  ],
-  reviews: [reviewSchema],
-});
+    description: {
+      type: String,
+      required: "You need to leave a description!",
+      minlength: 1,
+      maxlength: 500,
+      trim: true,
+    },
+    services: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Service",
+      },
+    ],
+    reviews: [reviewSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 companySchema.virtual("averageRating").get(function () {
   return this.reviews.length > 0
