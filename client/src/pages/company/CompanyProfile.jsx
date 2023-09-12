18c1c9b469ee5743538240c19f23bdf6c01c57be
holdenmg <holdenmgarrison@gmail.com>
2023-09-12 15:54:12 -0400

    Updated companyProfile to use QUERY_COMPANY

--- client/src/pages/company/CompanyProfile.jsx
+++ client/src/pages/company/CompanyProfile.jsx
@@ -8,7 +8,9 @@ import{ QUERY_COMPANY } from "../../utils/queries";
 export default function CompanyProfile() {
 
   const {companyId} = useParams();
-  const{data} = useQuery(QUERY_COMPANY, )
+  const{data} = useQuery(QUERY_COMPANY, {
+    variables: { companyId },
+  });
 
   
 
@@ -18,8 +20,8 @@ export default function CompanyProfile() {
           <div className="d-flex justify-content-between align-items-start">
             <div>
               <span>Company Profile</span>
-              <h2 className=' fs-1'>{ company._company.name }</h2>
-              <span>{ company._company.location }</span>
+              <h2 className=' fs-1'>{data.company.name }</h2>
+              <span>{ data.company.location }</span>
             </div>
 
             <div className="justify-content-end align-items-start">
@@ -27,22 +29,22 @@ export default function CompanyProfile() {
               <a href="/Messages" className="mx-2">Message</a>
               {/* Make button a component */}
               {/* Add a modal */}
-              <a class='btn green text-light' href='#' role='button'>Edit Profile</a>
+              <a className='btn green text-light' href='#' role='button'>Edit Profile</a>
             </div>
           </div>
           <div>
-            <p>{ company._company.description }</p>
+            <p>{ data.company.description }</p>
           </div>
           <hr/>
           <div>
             <h3 className="fs-5">Company Info</h3>
-            <p>email: { company.email }</p>
-            <p>password: { company.password }</p>
+            <p>email: { data.company.email }</p>
+            
           </div>
           <hr/>
           <div className="d-flex justify-content-between">
             <h3 className="fs-5">Reviews</h3>
-            <span> Total Reviews: { company._company.reviews.length } </span>
+            <span> Total Reviews: { data.company.reviews.length } </span>
           </div>
           <div>
             <ReviewCard />
