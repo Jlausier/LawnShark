b50306126d853d4b86863eed4a98e8ff60e01a5e
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-13 14:57:04 -0400

    shark logo

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -72,7 +72,7 @@ export default function Search() {
               ))}
             </div>
           ) : (
-            <div>no companies</div>
+            <div>Search results will appear here.</div>
           )}
         </div>
       </div>
--- client/src/pages/user/JobPostings.jsx
+++ client/src/pages/user/JobPostings.jsx
@@ -27,7 +27,7 @@ export default function JobPostings() {
               ))}
             </div>
           ) : (
-            <div>No job postings...</div>
+            <div>You have not made any new Job Postings.</div>
           )}
         </div>
       </div>
