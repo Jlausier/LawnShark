edaed777e16cb43597bbc67faa10e45caf5f4728
holdenmg <holdenmgarrison@gmail.com>
2023-09-13 19:58:07 -0400

    rewrote findWork page

--- client/src/pages/company/FindWork.jsx
+++ client/src/pages/company/FindWork.jsx
@@ -1,51 +1,89 @@
 /* eslint-disable no-unused-vars */
 import { useQuery } from "@apollo/client";
-
-import { QUERY_POSTINGS_FILTERED } from "../../utils/queries";
-
-import { useState } from "react"
+import { QUERY_SERVICES } from "../../utils/queries";
+import { useState, useEffect } from "react"; // Import useEffect
 
 import { usePostingsSearch } from "../../hooks/usePostings.js";
 import JobPostingCard from "../../components/JobPostingCard";
-import FilterCheckBox from "../../components/FilterCheckBox";
-
 
 export default function FindWork() {
+ 
+  const { serviceData } = useQuery(QUERY_SERVICES);
 
-    const { data } = useQuery(QUERY_POSTINGS_FILTERED, {
-      variables: {
-        services: "",
-      },
-    });
-    console.log(data);
+  // Initialize state for checked services
+  const [checkedState, setCheckedState] = useState({});
 
-  const postings = usePostingsSearch
+  const handleOnChange = (serviceId) => {
+    setCheckedState((prevState) => ({
+      ...prevState,
+      [serviceId]: !prevState[serviceId],
+    }));
+  };
+
+  // Use useEffect to watch for changes in checkedState and update searchOptions
+  useEffect(() => {
+    const selectedServices = Object.keys(checkedState).filter(
+      (serviceId) => checkedState[serviceId]
+    );
+
+    setSearchOptions({ services: selectedServices });
+  }, [checkedState]);
+
+  // Initialize state for search options
+  const [searchOptions, setSearchOptions] = useState({
+    services: [],
+  });
+
+  // Fetch postings based on search options
+  const postings = usePostingsSearch(searchOptions);
+
   return (
-    <div className=" p-5">
+    <div className="p-5">
       <div className="border p-4 rounded">
         <div className="mb-5">
-          <h2 className="header">Find Work</h2>
-          <span className="body-font">Click on job titles to view more details</span>
+          <h2 className="fs-1">Find Work</h2>
+          <span className="">Click on job titles to view more details</span>
         </div>
-        <div className="d-flex mb-3">
-          <span className="pe-2">Filter Results:</span>
-          <FilterCheckBox title={"Lawn Care"} value={""} />
-          <FilterCheckBox title={"Tree Trimming"} value={""} />
-          <FilterCheckBox title={"Pressure-Washing"} value={""} />
-          <FilterCheckBox title={"Fertilizing"} value={""} />
-        </div>
-        <div>
-          {data && data.Posting && data.Posting.length > 0 ? (
-            <div>
-              {data.Posting.map((postings) => (
-                <JobPostingCard {...postings} key={postings._id} />
-              ))}
-            </div>
-          ) : (
-            <div className="fs-4 body-font">There are no available job postings. Check again later!</div>
-          )}
-        </div>
+        {serviceData && serviceData.services && serviceData.services.length > 0 ? (
+          <div className="mb-3">
+            <span className="pe-2">Filter Results:</span>
+            {serviceData.services.map((service) => (
+              <div className="form-check" key={service._id}>
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={service._id}
+                  name={service.name}
+                  onChange={() => handleOnChange(service._id)}
+                  checked={checkedState[service._id]}
+                />
+                <label className="form-check-label" htmlFor={service._id}>
+                  {service.name}
+                </label>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div> NO SERVICES </div>
+        )}
       </div>
+      {postings && postings.postingsFiltered && postings.postingsFiltered.length > 0 ? (
+        <div>
+          {postings.postingsFiltered.map((posting) => (
+            <JobPostingCard {...posting} key={posting._id} />
+          ))}
+        </div>
+      ) : (
+        <div> No Postings </div>
+      )}
     </div>
   );
 }
+
+
+
+
+
+
+
+
