c2665e3ca906a824d79b7c17db29988541c581ec
arnald18 <acenaldy04@gmail.com>
2023-09-13 16:05:42 -0400

    add props to notification card

--- client/src/components/NotificationCard.jsx
+++ client/src/components/NotificationCard.jsx
@@ -1,26 +1,31 @@
+import PropTypes from "prop-types";
+
 const testNotification = {
   title: "Quote",
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
   source: "Source Title",
 };
 
-function NotificationCard() {
+function NotificationCard({ title, text, source }) {
   return (
     <div className="card">
-      <div className="card-header">Quote</div>
+      <div className="card-header">{title}</div>
       <div className="card-body">
         <blockquote className="blockquote mb-0">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            posuere erat a ante.
-          </p>
+          <p>{text}</p>
           <footer className="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
+            FROM <cite title="Source Title">{source}</cite>
           </footer>
         </blockquote>
       </div>
     </div>
   );
 }
+
+NotificationCard.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  source: PropTypes.string,
+};
 
 export default NotificationCard;
