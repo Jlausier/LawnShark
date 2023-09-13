c91190aab0885e8faed47b976c61e599c90addac
Brian Gorman <briangorman99@gmail.com>
2023-09-13 19:52:12 -0400

    Update UpdateForm.jsx
    fix label and input names

--- client/src/components/modal/UpdateForm.jsx
+++ client/src/components/modal/UpdateForm.jsx
@@ -1,10 +1,9 @@
 export default function UpdateForm({ title, onUpdate }) {
-
     return (
         <form className="row">
-        <label htmlFor="exampleInputPassword1" className="form-label">{title}</label>
+        <label htmlFor={title} className="form-label">{title}</label>
         <div className="mb-3 col-9">
-          <input type="text" className="form-control" id="exampleInputPassword1" onChange={onUpdate} />
+          <input type="text" className="form-control" id={title} name={title} onChange={onUpdate} />
         </div>
         <div className="col">
           <button type="submit" className="btn btn-sm btn-success">Update</button>
