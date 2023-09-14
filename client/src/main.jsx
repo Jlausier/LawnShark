7660045080a3fb7d8b014e77b4d1772ac4444b9b
Brian Gorman <briangorman99@gmail.com>
2023-09-14 04:26:44 -0400

    remove RoleContext and RoleProvider, update exports from page folders

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,31 +2,23 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import client from "./utils/apolloClient.js";
-import RoleProvider from "./utils/RoleProvider.jsx";
 
 import App from "./App.jsx";
 import SplashPage from "./pages/SplashPage.jsx";
 
-import JobPosting from "./pages/private/JobPosting.jsx";
-
-import FindWork from "./pages/company/FindWork.jsx";
-import MyJobs from "./pages/company/MyJobs.jsx";
-import MyBids from "./pages/company/MyBids.jsx";
-import CompanyProfile from "./pages/private/CompanyProfile.jsx";
-
-import JobPostings from "./pages/customer/JobPostings.jsx";
-import CreateJobPosting from "./pages/customer/CreateJobPosting.jsx";
-import UserProfile from "./pages/customer/UserProfile.jsx";
-
-import Search from "./pages/company/Search.jsx";
-import Notifications from "./pages/private/Notifications.jsx";
-import Messages from "./pages/private/Messages.jsx";
-
-import "./index.css";
+import { FindWork, MyBids, MyJobs, Search } from "./pages/company";
+import { CreateJobPosting, JobPostings, UserProfile } from "./pages/customer";
+import {
+  CompanyProfile,
+  JobPosting,
+  Messages,
+  Notifications,
+} from "./pages/private";
 import PrivateRoute from "./pages/PrivateRoute.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const privateRoutes = [
   {
@@ -93,9 +85,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RoleProvider>
-    <ApolloProvider client={client}>
-      <RouterProvider router={router} />
-    </ApolloProvider>
-  </RoleProvider>
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
 );
