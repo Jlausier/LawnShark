8f75d870e607d33695c0ff5066ad0e2bd17fa1f9
Brian Gorman <briangorman99@gmail.com>
2023-09-14 20:10:28 -0400

    extract state constants from component, combine form state, update handleFormSubmit, update handleInputChange

--- client/src/components/auth-form/CustomerForm.jsx
+++ client/src/components/auth-form/CustomerForm.jsx
@@ -2,67 +2,78 @@ import { useState } from "react";
 import { validatePassword, validateEmail } from "../../utils/auth";
 import Button from "../Button";
 
+const stateNames = [
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming",
+];
+
+const initialFormState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+};
+
+const stateOptions = stateNames.map((state) => (
+  <option key={state} value={state}>
+    {state}
+  </option>
+));
+
 export default function CustomerForm() {
   const [selectedState, setSelectedState] = useState("");
-
-  const stateNames = [
-    "Alabama",
-    "Alaska",
-    "Arizona",
-    "Arkansas",
-    "California",
-    "Colorado",
-    "Connecticut",
-    "Delaware",
-    "Florida",
-    "Georgia",
-    "Hawaii",
-    "Idaho",
-    "Illinois",
-    "Indiana",
-    "Iowa",
-    "Kansas",
-    "Kentucky",
-    "Louisiana",
-    "Maine",
-    "Maryland",
-    "Massachusetts",
-    "Michigan",
-    "Minnesota",
-    "Mississippi",
-    "Missouri",
-    "Montana",
-    "Nebraska",
-    "Nevada",
-    "New Hampshire",
-    "New Jersey",
-    "New Mexico",
-    "New York",
-    "North Carolina",
-    "North Dakota",
-    "Ohio",
-    "Oklahoma",
-    "Oregon",
-    "Pennsylvania",
-    "Rhode Island",
-    "South Carolina",
-    "South Dakota",
-    "Tennessee",
-    "Texas",
-    "Utah",
-    "Vermont",
-    "Virginia",
-    "Washington",
-    "West Virginia",
-    "Wisconsin",
-    "Wyoming",
-  ];
-
-  const stateOptions = stateNames.map((state) => (
-    <option key={state} value={state}>
-      {state}
-    </option>
-  ));
 
   const handleStateChange = (e) => {
     setSelectedState(e.target.value);
@@ -71,63 +82,41 @@ export default function CustomerForm() {
   const handleSubmit = () => {
     alert("Submit Form");
   };
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [fname, setFname] = useState("");
-  const [lname, setLname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [street, setStreet] = useState("");
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
-  const [zip, setZip] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+
+  const [formState, setFormState] = useState(initialFormState);
+  const [errorMessages, setErrorMessages] = useState({});
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputName = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputName === "fname") {
-      setFname(inputValue);
-    } else if (inputName === "lname") {
-      setLname(inputValue);
-    } else if (inputName === "email") {
-      setEmail(inputValue);
-    } else if (inputName === "password") {
-      setPassword(inputValue);
-    } else if (inputName === "street") {
-      setStreet(inputValue);
-    } else if (inputName === "city") {
-      setCity(inputValue);
-    } else if (inputName === "state") {
-      setState(inputValue);
-    } else {
-      setZip(inputValue);
-    }
+    const { name: inputName, value } = e.target;
+    setFormState({
+      ...formState,
+      [inputName]: value,
+    });
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email)) {
-      setErrorMessage("Email is invalid");
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    const errors = {};
+
+    if (formState.email === "") {
+      errors.email = "Email is required";
+    } else if (!validateEmail(formState.email)) {
+      errors.email = "Email is invalid";
     }
-    if (!validatePassword(password)) {
-      setErrorMessage(`Choose a more secure password for the account.`);
+
+    if (formState.password === "") {
+      errors.password = "Password is required";
+    } else if (!validatePassword(formState.password)) {
+      errors.password = "Choose a more secure password for the account";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setErrorMessages(errors);
       return;
     }
 
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setPassword("");
-    setEmail("");
+    setFormState(initialFormState);
   };
 
   return (
@@ -135,12 +124,13 @@ export default function CustomerForm() {
       <div className="row mb-4">
         <div className="col-12 col-lg-6">
           <div className="">
-            <label htmlFor="emailInput" className="form-label body-font">
+            <label htmlFor="email" className="form-label body-font">
               Email Address
             </label>
             <input
-              value={email}
+              value={formState.email}
               name="email"
+              id="email"
               onChange={handleInputChange}
               type="email"
               className="form-control"
@@ -149,12 +139,13 @@ export default function CustomerForm() {
         </div>
         <div className="col-12 col-lg-6">
           <div className="">
-            <label htmlFor="passwordInput" className="form-label body-font">
+            <label htmlFor="password" className="form-label body-font">
               Password
             </label>
             <input
-              value={password}
+              value={formState.password}
               name="password"
+              id="password"
               onChange={handleInputChange}
               type="password"
               className="form-control"
@@ -165,12 +156,13 @@ export default function CustomerForm() {
       <div className="row mb-4">
         <div className="col-12 col-md-6">
           <div className="">
-            <label htmlFor="emailInput" className="form-label body-font">
+            <label htmlFor="firstName" className="form-label body-font">
               First Name
             </label>
             <input
-              value={fname}
-              name="fname"
+              value={formState.firstName}
+              name="firstName"
+              id="firstName"
               onChange={handleInputChange}
               type="text"
               className="form-control"
@@ -179,12 +171,13 @@ export default function CustomerForm() {
         </div>
         <div className="col-12 col-md-6">
           <div className="">
-            <label htmlFor="emailInput" className="form-label body-font">
+            <label htmlFor="lastName" className="form-label body-font">
               Last Name
             </label>
             <input
-              value={lname}
-              name="lname"
+              value={formState.lastName}
+              name="lastName"
+              id="lastName"
               onChange={handleInputChange}
               type="text"
               className="form-control"
@@ -195,12 +188,13 @@ export default function CustomerForm() {
       <div className="">
         <div className="row mb-2">
           <div className="col">
-            <label htmlFor="streetInput" className="form-label body-font">
+            <label htmlFor="street" className="form-label body-font">
               Street
             </label>
             <input
-              value={street}
+              value={formState.street}
               name="street"
+              id="street"
               onChange={handleInputChange}
               type="text"
               className="form-control"
@@ -209,38 +203,42 @@ export default function CustomerForm() {
         </div>
         <div className="row mb-4">
           <div className="col-12 col-lg-5">
-            <label htmlFor="cityInput" className="form-label body-font">
+            <label htmlFor="city" className="form-label body-font">
               City
             </label>
             <input
-              value={city}
+              value={formState.city}
               name="city"
+              id="city"
               onChange={handleInputChange}
               type="text"
               className="form-control"
             />
           </div>
           <div className="col-12 col-lg-4">
-            <label htmlFor="stateInput" className="form-label body-font">
+            <label htmlFor="state" className="form-label body-font">
               State
             </label>
             <select
               value={selectedState}
               onChange={handleStateChange}
               className="form-control"
+              name="state"
+              id="state"
             >
-              <option value="">Select a state</option>{" "}
               {/* Default empty option */}
+              <option value="">Select a state</option>
               {stateOptions}
             </select>
           </div>
           <div className="col-12 col-lg-3">
-            <label htmlFor="zipInput" className="form-label body-font">
+            <label htmlFor="zip" className="form-label body-font">
               Zip
             </label>
             <input
-              value={zip}
+              value={formState.zip}
               name="zip"
+              id="zip"
               onChange={handleInputChange}
               type="text"
               className="form-control"
