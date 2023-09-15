6f464828e9902bf49ee3a89b9a9da7cbcf525119
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-09-15 01:08:43 -0400

    added hover effect and fixed placement of new job posting

--- client/src/components/JobPostingCard.jsx
+++ client/src/components/JobPostingCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
@@ -8,18 +9,37 @@ export default function JobPostingCard({
   frequency,
   description,
 }) {
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const linkStyle = {
+    textDecoration: "none",
+    color: "inherit",
+    transition: "0.3s"
+  };
+
+  const linkHoverStyle = {
+    backgroundColor: "#d2f7d5"
+  }
+
   return (
-    <Link to={`/JobPosting/${_id}`} className="mb-3 card w-100">
+    <Link 
+      to={`/JobPosting/${_id}`} 
+      style={isHovered ? { ...linkStyle, ...linkHoverStyle } : linkStyle}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="mb-3 card w-100"
+    >
       <div className="card-body">
         <div className="row mt-2 mb-4">
           <div className="col-6">
-            <h5 className="card-title fs-2">{title}</h5>
+            <h5 className="card-title body-font fs-2">{title}</h5>
             <h6 className="card-subtitle mb-2 text-muted">
               {/* {customer.location} */}
             </h6>
           </div>
           <div className="col-6 text-end d-flex flex-column align-items-end">
-            <span className="px-4 py-2 rounded green text-light fs-5">
+            <span className="px-4 py-2 rounded green body-font text-light fs-5">
               ${askingPrice}
             </span>
             <span>{frequency}</span>
