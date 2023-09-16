5346a4c9765adbe79be8e830eb1ca2e270a90bee
Brian Gorman <briangorman99@gmail.com>
2023-09-15 23:08:56 -0400

    add useRemoveBid hook

---
+++ client/src/hooks/useRemoveBid.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@apollo/client";
+
+import { REMOVE_BID } from "../utils/mutations";
+
+export default function useRemoveBid() {
+  const [_removeBid, { error }] = useMutation(REMOVE_BID);
+
+  async function removeBid(bidId) {
+    try {
+      const { data } = await _removeBid({
+        variables: { bidId },
+      });
+
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return { removeBid, error };
+}
