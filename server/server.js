e7a98c5d7a29b4aa58a01bca971a8774b10ce34f
Brian Gorman <briangorman99@gmail.com>
2023-09-22 19:30:10 -0400

    add build script to main package.json

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const startApolloServer = async () => {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("*", (_, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      res.sendFile(path.join(__dirname, "/../client/dist/index.html"));
     });
   }
 
