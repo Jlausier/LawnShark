2fd56a383cc2e6a52381e473a0110f24750cb279
Brian Gorman <briangorman99@gmail.com>
2023-09-26 23:45:52 -0400

    add loading variable to usePostingsSearch results

--- client/src/hooks/usePostings.js
+++ client/src/hooks/usePostings.js
@@ -3,8 +3,8 @@ import { useQuery } from "@apollo/client";
 import { QUERY_POSTINGS_FILTERED } from "../utils/queries";
 
 export function usePostingsSearch(variables) {
-  const { data } = useQuery(QUERY_POSTINGS_FILTERED, {
+  const { data, loading } = useQuery(QUERY_POSTINGS_FILTERED, {
     variables,
   });
-  return data;
+  return { data, loading };
 }
